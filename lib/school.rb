91570e8483e3a1a9f4e4d61a154b60fe9bfbfb45
Britny Edwards <bredwards2@gmail.com>
2018-12-16 22:12:19 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+school =
+School.new("Bayside High School")
+
+school.roster
+
+school.add_student("Zach Morris", 9)
+school.roster
+
+hash = { }
+hash["new key"] << "new_value_for_value_array"
+
+hash => {"new key"=> ["new_value_for_value_array"]}
+
+school.add_student("AC Slater", 9)
+school.add_student("Kelly Kapowski", 10)
+school.add_student("Screech", 11)
+
+school.grade(9)
+
+school.skip_before_filter
